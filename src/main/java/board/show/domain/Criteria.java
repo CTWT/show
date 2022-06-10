33752934e5f6784abab7ed929f91a1dd784602b4
CTWT <inchul9603@naver.com>
2022-06-10 21:25:02 +0900

    paging 제작 v0.1
    paging 제작 진행

---
+++ src/main/java/board/show/domain/Criteria.java
@@ -0,0 +1,23 @@
+package board.show.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Criteria {     // paging 만들기
+
+    private int pageNum;
+    private int amount;
+
+    public Criteria() {
+        this(1, 10);
+    }
+
+    public Criteria(int pageNum, int amount) {
+        this.pageNum = pageNum;
+        this.amount = amount;
+    }
+}
